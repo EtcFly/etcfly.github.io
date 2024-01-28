99191ff2f1797595a28fcb49459fcc722a0aaf30
etcfly <etcflywy@126.com>
2024-01-28 20:30:25 +0800

    Site updated: 2024-01-28 20:30:22

---
+++ live2d-widget/autoload.js
@@ -0,0 +1,19 @@
+const live2d_path="/live2d-widget/";function loadExternalResource(o,s){return new Promise((e,t)=>{let i;"css"===s?((i=document.createElement("link")).rel="stylesheet",i.href=o):"js"===s&&((i=document.createElement("script")).src=o),i&&(i.onload=()=>e(o),i.onerror=()=>t(o),document.head.appendChild(i))})}768<=screen.width&&Promise.all([loadExternalResource(live2d_path+"waifu.css","css"),loadExternalResource(live2d_path+"live2d.min.js","js"),loadExternalResource(live2d_path+"waifu-tips.js","js")]).then(()=>{initWidget({waifuPath:live2d_path+"waifu-tips.json",apiPath:"https://etcfly.top:5000/api/",tools:["hitokoto","asteroids","switch-model","switch-texture","photo","info","quit"]})}),console.log(`
+  く__,.ヘヽ.        /  ,ー､ 〉
+           ＼ ', !-─‐-i  /  /´
+           ／｀ｰ'       L/／｀ヽ､
+         /   ／,   /|   ,   ,       ',
+       ｲ   / /-‐/  ｉ  L_ ﾊ ヽ!   i
+        ﾚ ﾍ 7ｲ｀ﾄ   ﾚ'ｧ-ﾄ､!ハ|   |
+          !,/7 '0'     ´0iソ|    |
+          |.从"    _     ,,,, / |./    |
+          ﾚ'| i＞.､,,__  _,.イ /   .i   |
+            ﾚ'| | / k_７_/ﾚ'ヽ,  ﾊ.  |
+              | |/i 〈|/   i  ,.ﾍ |  i  |
+             .|/ /  ｉ：    ﾍ!    ＼  |
+              kヽ>､ﾊ    _,.ﾍ､    /､!
+              !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+              ﾚ'ヽL__|___i,___,ンﾚ|ノ
+                  ﾄ-,/  |___./
+                  'ｰ'    !_,.:
+`);
